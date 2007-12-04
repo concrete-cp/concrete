fa170cc1d0b2df3e926e0c9e227304573886fe1b
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2007-12-04 14:29:45 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspfj@107 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

---
+++ src/cspfj/filter/AbstractSAC.java
@@ -0,0 +1,88 @@
+/*
+ * Created on 4 déc. 07
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package cspfj.filter;
+
+import java.util.logging.Logger;
+
+import cspfj.problem.Problem;
+import cspfj.problem.Variable;
+
+public abstract class AbstractSAC implements Filter {
+
+	protected final Filter filter;
+	protected final Problem problem;
+	protected int nbSingletonTests = 0;
+	
+	private static final Logger logger = Logger.getLogger(AbstractSAC.class.toString());
+
+	public AbstractSAC(Problem problem, Filter filter) {
+		super();
+		this.filter = filter;
+		this.problem = problem;
+	}
+
+	public boolean reduceAfter(final int level, final Variable variable) {
+		if (variable == null) {
+			return true;
+		}
+		return reduceAll(level);
+	}
+
+	protected abstract boolean singletonTest(Variable variable, int level);
+	
+	protected boolean reduce(final int level) {
+		final Problem problem = this.problem;
+
+		final Filter filter = this.filter;
+
+		if (!filter.reduceAll(level)) {
+			return false;
+		}
+
+		final Variable[] variables = problem.getVariables();
+
+		int mark = 0;
+
+		int v = 0;
+
+		do {
+			final Variable variable = variables[v];
+
+			if (singletonTest(variable, level)) {
+				if (variable.getDomainSize() <= 0) {
+					return false;
+				}
+				if (!filter.reduceAfter(level, variable)) {
+					return false;
+				}
+				mark = v;
+			}
+
+			v = next(v, variables.length);
+		} while (v != mark);
+
+		return true;
+
+	}
+	
+	public boolean reduceAll(final int level) {
+		return reduce(level);
+	}
+
+	public int getNbSingletonTests() {
+		return nbSingletonTests;
+	}
+	
+	public static int next(final int i, final int length) {
+		if (i < length - 1) {
+			return i + 1;
+		}
+		logger.info("Tour !");
+		return 0;
+	}
+
+}
--- src/cspfj/filter/CDC.java
+++ src/cspfj/filter/CDC.java
@@ -28,123 +28,57 @@ import cspfj.problem.Variable;
  * @author Julien VION
  * 
  */
-public final class CDC implements Filter {
-
-	private final Filter filter;
+public final class CDC extends AbstractSAC {
 
 	private final static Logger logger = Logger.getLogger("cspfj.filter.CDC");
 
-	private final Problem problem;
-
 	private int nbNoGoods = 0;
 
-	private int nbSingletonTests = 0;
-
 	public CDC(Problem problem, Filter filter) {
-		super();
-		this.filter = filter;
-		this.problem = problem;
+		super(problem, filter);
 	}
 
-	private int next(final int i, final int length) {
-		if (i < length - 1) {
-			return i + 1;
-		}
-		logger.info("Tour !");
-		return 0;
-	}
+	protected boolean singletonTest(Variable variable, int level) {
+		boolean changedGraph = false;
+		for (int index = variable.getFirst(); index >= 0; index = variable
+				.getNext(index)) {
 
-	private boolean reduce(final int level) {
-		final Problem problem = this.problem;
-
-		logger.info("CDC");
-
-		final Filter filter = this.filter;
-
-		if (!filter.reduceAll(level)) {
-			return false;
-		}
-
-		final Variable[] variables = problem.getVariables();
-
-		int mark = 0;
-
-		int v = 0;
-
-		do {
-			final Variable variable = variables[v];
-
-			for (int index = variable.getFirst(); index >= 0; index = variable
-					.getNext(index)) {
-
-				boolean changedGraph = false;
-				
-				if (!variable.isPresent(index)) {
-					continue;
-				}
-
-				if (logger.isLoggable(Level.FINE)) {
-					logger.fine(level + " : " + variable + " <- "
-							+ variable.getDomain()[index] + "(" + index + ")");
-				}
-
-				variable.assign(index, problem);
-				problem.setLevelVariables(level, variable);
-				nbSingletonTests++;
-				if (filter.reduceAfter(level + 1, variable)) {
-					final int nbNg = problem.addNoGoods();
-					nbNoGoods += nbNg;
-
-					changedGraph = nbNg > 0;
-
-					variable.unassign(problem);
-					problem.restore(level + 1);
-				} else {
-					variable.unassign(problem);
-					problem.restore(level + 1);
-					logger.fine("Removing " + variable + ", " + index);
-
-					variable.remove(index, level);
-					changedGraph = true;
-
-					if (variable.getDomainSize() <= 0) {
-						return false;
-					}
-				}
-
-				if (changedGraph) {
-					if (!filter.reduceAfter(level, variable)) {
-						return false;
-					}
-					mark = v;
-				}
+			if (!variable.isPresent(index)) {
+				continue;
 			}
-			v = next(v, variables.length);
-		} while (v != mark);
-//		logger.info("Total : " + Problem.noGoodTime);
-//		logger.info("Find Constraint : " + Problem.findConstraintTime);
-//		logger.info("Remove Tuples : " + Problem.removeTupleTime);
-		return true;
 
-	}
+			if (logger.isLoggable(Level.FINE)) {
+				logger.fine(level + " : " + variable + " <- "
+						+ variable.getDomain()[index] + "(" + index + ")");
+			}
 
-	public boolean reduceAfter(final int level, final Variable variable) {
-		if (variable == null) {
-			return true;
+			variable.assign(index, problem);
+			problem.setLevelVariables(level, variable.getId());
+			nbSingletonTests++;
+			if (filter.reduceAfter(level + 1, variable)) {
+				final int nbNg = problem.addNoGoods();
+				nbNoGoods += nbNg;
+
+				changedGraph = nbNg > 0;
+
+				variable.unassign(problem);
+				problem.restore(level + 1);
+			} else {
+				variable.unassign(problem);
+				problem.restore(level + 1);
+				logger.fine("Removing " + variable + ", " + index);
+
+				variable.remove(index, level);
+				changedGraph = true;
+			}
 		}
-		return reduceAll(level);
+		return changedGraph;
 	}
 
-	public boolean reduceAll(final int level) {
-		return reduce(level);
-	}
+
 
 	public int getNbNoGoods() {
 		return nbNoGoods;
-	}
-
-	public int getNbSingletonTests() {
-		return nbSingletonTests;
 	}
 
 	public String toString() {
