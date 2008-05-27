83455d26d189671ef13cd87bddc43397038db31a
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2008-05-27 14:14:54 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspfj@167 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/cspfj/constraint/TupleListCraftedSet.java
+++ src/cspfj/constraint/TupleListCraftedSet.java
@@ -1,76 +1,108 @@
 package cspfj.constraint;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class TupleListCraftedSet implements Matrix, Cloneable {
 
-	private Map<Long, Collection<int[]>> list;
+	// private Map<Long, Collection<int[]>> list;
 
 	private final boolean initialContent;
 
-	public TupleListCraftedSet(final int[] sizes, final boolean initialContent) {
+	private final Cell[] hashTable;
+
+	// //private int size;
+
+	public static int collisions = 0;
+
+	public static int tuples = 0;
+	
+	private final int hashTableSize ;
+
+	private final static int[] primes55 = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+			31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+			103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+			173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239,
+			241, 251, 257 };
+
+	public TupleListCraftedSet(final int[] sizes, final boolean initialContent,
+			final int nbTuples) {
 		this.initialContent = initialContent;
-		list = new HashMap<Long, Collection<int[]>>();
+		// list = new HashMap<Long, Collection<int[]>>();
+		// this.size = 0;
+		hashTableSize = firstPrimeAfter(nbTuples*4/3);
+		hashTable = new Cell[hashTableSize];
+		
 	}
 
-	private static long hash(final int[] tuple) {
-		long hash = 3581;
-		for (int i = tuple.length; --i >= 0;) {
-			hash = (hash * 33) ^ tuple[i];
+	private static int firstPrimeAfter(int value) {
+		int test = value - 1;
+		while (!isPrime(++test))
+			;
+		return test;
+	}
+
+	private static boolean isPrime(int value) {
+		for (int prime : primes55) {
+			if (value % prime == 0) {
+				if (value == prime) {
+					return true;
+				} else {
+					return false;
+				}
+			}
 		}
-		return hash;
+
+		final int maxtest = value / 4;
+
+		for (int i = 259; i < maxtest; i += 2) {
+			if (value % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+	private static int hash(final int[] tuple) {
+		int hash = -2128831035;
+		for (int i : tuple) {
+			hash *= 16777619;
+			hash ^= i;
+
+		}
+		return hash<0?(-hash)^0x1:hash;
 	}
 
 	@Override
 	public boolean check(final int[] tuple) {
-		return contains(tuple, list.get(hash(tuple))) ^ initialContent;
-	}
-
-	private static boolean contains(final int[] tuple,
-			final Collection<int[]> array) {
-		if (array == null) {
-			return false;
-		}
-		for (int[] t : array) {
-			if (Arrays.equals(t, tuple)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private static void remove(final int[] tuple, final Collection<int[]> array) {
-		if (array == null) {
-			return;
-		}
-		final Iterator<int[]> itr = array.iterator();
-
-		while (itr.hasNext()) {
-			if (Arrays.equals(itr.next(), tuple)) {
-				itr.remove();
-				break;
-			}
-		}
+		final Cell cell = hashTable[hash(tuple) % hashTableSize];
+		return cell == null ? initialContent : cell.contains(tuple)
+				^ initialContent;
 	}
 
 	@Override
 	public void set(final int[] tuple, final boolean status) {
 		if (status == initialContent) {
-			remove(tuple, list.get(hash(tuple)));
+			hashTable[hash(tuple) % hashTableSize] = hashTable[hash(tuple)
+					% hashTableSize].remove(tuple);
 		} else {
-			Collection<int[]> localList = list.get(hash(tuple));
-			if (localList == null) {
-				localList = new LinkedList<int[]>();
-				list.put(hash(tuple), localList);
+			Cell cell = hashTable[hash(tuple) % hashTableSize];
+			if (cell == null) {
+				cell = new Cell(tuple.clone());
+				tuples++;
+				// size++;
+			} else {
+//				if (!cell.contains(tuple)) {
+					cell = cell.add(tuple.clone());
+					// size++;
+					tuples++;
+					collisions++;
+//				}
 			}
-			if (!contains(tuple, localList)) {
-				localList.add(tuple.clone());
-			}
+			hashTable[hash(tuple) % hashTableSize] = cell;
 		}
 	}
 
@@ -81,41 +113,100 @@ public class TupleListCraftedSet implements Matrix, Cloneable {
 
 		return list;
 	}
-	//
 
-	//
-	// final static BigInteger sfactor = BigInteger.valueOf(33);
-	//	
-	// private static BigInteger hashBig(final int[] tuple) {
-	// BigInteger hash = BigInteger.ONE;
-	//
-	// for (int i = tuple.length; --i >= 0;) {
-	// hash = hash.multiply(sfactor).add(BigInteger.valueOf(tuple[i]));
-	// }
-	//
-	// return hash;
-	// }
-	//
-	// public static void main(String[] args) {
-	// final int[] tuple = new int[Integer.valueOf(args[0])];
-	// for (int i = tuple.length ; --i>=0;) {
-	// tuple[i]=Integer.valueOf(args[1])+i;
-	// }
-	// final int tests = Integer.valueOf(args[2]);
-	// long start = -CpuMonitor.getCpuTimeNano();
-	// for (int i = tests ; --i>=0;) {
-	// hashBig(tuple);
-	// }
-	// start += CpuMonitor.getCpuTimeNano();
-	// System.out.println("Big : " + start/1e9d);
-	//		
-	// start = -CpuMonitor.getCpuTimeNano();
-	// for (int i = tests ; --i>=0;) {
-	// hashInt(tuple);
-	// }
-	// start += CpuMonitor.getCpuTimeNano();
-	// System.out.println("long : " + start/1e9d);
-	//		
-	// }
-	//	
+	private static class Cell {
+		private final int[] tuple;
+		private Cell next;
+
+		public Cell(final int[] tuple) {
+			this.tuple = tuple;
+			this.next = null;
+		}
+
+		public Cell add(final int[] tuple) {
+			final Cell cell = new Cell(tuple);
+			cell.next = this;
+			return cell;
+		}
+
+		public boolean contains(final int[] tuple) {
+			if (Arrays.equals(this.tuple, tuple)) {
+				return true;
+			}
+			if (next == null) {
+				return false;
+			}
+			return next.contains(tuple);
+		}
+
+		public Cell remove(final int[] tuple) {
+			if (Arrays.equals(this.tuple, tuple)) {
+				return next;
+			}
+			next = next.remove(tuple);
+			return this;
+		}
+
+		public String toString() {
+			if (next == null) {
+				return Arrays.toString(tuple);
+			}
+			return next.toString() + "," + Arrays.toString(tuple);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		final Map<Integer, Integer> hashs = new HashMap<Integer, Integer>();
+		int collisions = 0;
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < 20; j++) {
+				for (int k = 0; k < 20; k++) {
+					final int key = hash(new int[] { i, j, k })
+							% firstPrimeAfter((int) (20 * 20 * 30));
+					if (!hashs.containsKey(key)) {
+						hashs.put(key, 1);
+					} else {
+						collisions++;
+						hashs.put(key, hashs.get(key) + 1);
+					}
+				}
+			}
+
+		}
+		System.out.println(collisions);
+		for (int i : hashs.keySet()) {
+			System.out.print(i + ":" + hashs.get(i) + " ");
+		}
+		System.out.println();
+
+		Cell cell = new Cell(new int[] { 1, 1, 1 });
+
+		System.out.println(cell);
+
+		cell = cell.add(new int[] { 2, 2, 2 });
+
+		System.out.println(cell);
+
+		cell = cell.add(new int[] { 3, 3, 3 });
+		cell = cell.add(new int[] { 4, 4, 4 });
+		cell = cell.add(new int[] { 5, 5, 5 });
+		cell = cell.add(new int[] { 6, 6, 6 });
+		System.out.println(cell);
+
+		System.out.println(cell.contains(new int[] { 3, 3, 3 }));
+		System.out.println(cell.contains(new int[] { 2, 3, 3 }));
+
+		cell = cell.remove(new int[] { 3, 3, 3 });
+		System.out.println(cell);
+		cell = cell.remove(new int[] { 1, 1, 1 });
+		System.out.println(cell);
+		cell = cell.remove(new int[] { 6, 6, 6 });
+		System.out.println(cell);
+
+		System.out.println(Integer.toBinaryString(-2128831035>>31));
+		int i = (int) 2166136261l;
+		System.out.println(i>>31);
+	}
+
 }
