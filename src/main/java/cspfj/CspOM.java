bd98ebd1ba48c48e471df1bae05b900cadb980ee
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2010-02-23 17:19:08 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspfj@417 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/java/cspfj/CspOM.java
+++ src/main/java/cspfj/CspOM.java
@@ -15,69 +15,69 @@ import cspom.constraint.CSPOMConstraint;
 import cspom.variable.CSPOMVariable;
 
 public class CspOM {
-    private final Problem problem;
-    private final Map<CSPOMVariable, Variable> variableMap = new HashMap<CSPOMVariable, Variable>();
+	private final Problem problem;
+	private final Map<CSPOMVariable, Variable> variableMap = new HashMap<CSPOMVariable, Variable>();
 
-    private CspOM(final Problem problem) {
-        this.problem = problem;
-    }
+	private CspOM(final Problem problem) {
+		this.problem = problem;
+	}
 
-    public static Problem generate(cspom.CSPOM cspom)
-            throws FailedGenerationException {
-        final CspOM generator = new CspOM(new Problem());
-        generator.gen(cspom);
-        return generator.problem;
-    }
+	public static Problem generate(cspom.CSPOM cspom)
+			throws FailedGenerationException {
+		final CspOM generator = new CspOM(new Problem());
+		generator.gen(cspom);
+		return generator.problem;
+	}
 
-    private void gen(cspom.CSPOM cspom) throws FailedGenerationException {
-        final Deque<CSPOMConstraint> constraintsQueue = new LinkedList<CSPOMConstraint>(
-                cspom.getConstraints());
+	private void gen(cspom.CSPOM cspom) throws FailedGenerationException {
+		final Deque<CSPOMConstraint> constraintsQueue = new LinkedList<CSPOMConstraint>(
+				cspom.getConstraints());
 
-        CSPOMConstraint firstSkippedConstraint = null;
+		CSPOMConstraint firstSkippedConstraint = null;
 
-        while (!constraintsQueue.isEmpty()) {
-            CSPOMConstraint cspomConstraint = constraintsQueue.poll();
-            Constraint cspfjConstraint;
-            try {
-                cspfjConstraint = ConstraintManager.generate(cspomConstraint
-                        .getDescription(), obtainScope(cspomConstraint));
-            } catch (Exception e) {
-                throw new FailedGenerationException(e);
-            }
+		while (!constraintsQueue.isEmpty()) {
+			CSPOMConstraint cspomConstraint = constraintsQueue.poll();
+			Constraint cspfjConstraint;
+			try {
+				cspfjConstraint = ConstraintManager.generate(cspomConstraint
+						.getDescription(), obtainScope(cspomConstraint));
+			} catch (Exception e) {
+				throw new FailedGenerationException(e);
+			}
 
-            if (cspfjConstraint == null) {
-                constraintsQueue.offer(cspomConstraint);
-                if (firstSkippedConstraint == null) {
-                    firstSkippedConstraint = cspomConstraint;
-                } else if (firstSkippedConstraint == cspomConstraint) {
-                    throw new FailedGenerationException(
-                            "Could not generate constraints: "
-                                    + constraintsQueue);
-                }
-            } else {
-                firstSkippedConstraint = null;
-                problem.addConstraint(cspfjConstraint);
-            }
-        }
-    }
+			if (cspfjConstraint == null) {
+				constraintsQueue.offer(cspomConstraint);
+				if (firstSkippedConstraint == null) {
+					firstSkippedConstraint = cspomConstraint;
+				} else if (firstSkippedConstraint == cspomConstraint) {
+					throw new FailedGenerationException(
+							"Could not generate constraints: "
+									+ constraintsQueue);
+				}
+			} else {
+				firstSkippedConstraint = null;
+				problem.addConstraint(cspfjConstraint);
+			}
+		}
+	}
 
-    private Variable[] obtainScope(CSPOMConstraint constraint) {
-        final Variable[] scope = new Variable[constraint.getArity()];
-        for (int i = constraint.getArity(); --i >= 0;) {
-            scope[i] = variableMap.get(constraint.getScope()[i]);
-        }
-        return scope;
-    }
+	private Variable[] obtainScope(CSPOMConstraint constraint) {
+		final Variable[] scope = new Variable[constraint.getArity()];
+		for (int i = constraint.getArity(); --i >= 0;) {
+			scope[i] = variableMap.get(constraint.getScope().get(i));
+		}
+		return scope;
+	}
 
-    private Variable var(CSPOMVariable cspomV, Problem problem,
-            Map<CSPOMVariable, Variable> map) {
-        Variable var = map.get(cspomV);
-        if (var == null) {
-            var = new Variable(null);
-            problem.addVariable(var);
-            map.put(cspomV, var);
-        }
-        return var;
-    }
+	private Variable var(CSPOMVariable cspomV, Problem problem,
+			Map<CSPOMVariable, Variable> map) {
+		Variable var = map.get(cspomV);
+		if (var == null) {
+			var = new Variable(null);
+			problem.addVariable(var);
+			map.put(cspomV, var);
+		}
+		return var;
+	}
 
 }
