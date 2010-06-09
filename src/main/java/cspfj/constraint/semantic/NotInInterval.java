1fc5ebaea6988890a123c978229e39dd426d03de
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2010-06-09 16:34:39 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspfj@510 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/java/cspfj/constraint/semantic/NotInInterval.java
+++ src/main/java/cspfj/constraint/semantic/NotInInterval.java
@@ -7,65 +7,56 @@ import cspfj.problem.Variable;
 
 public final class NotInInterval extends AbstractConstraint {
 
-	private final int lb, ub;
-	private final Domain domain;
+    private final int lb, ub;
+    private final Domain domain;
 
-	public NotInInterval(final Variable variable, final int lb, final int ub) {
-		super(variable);
+    public NotInInterval(final Variable variable, final int lb, final int ub) {
+        super(variable);
 
-		this.domain = variable.getDomain();
-		this.lb = domain.index(lb);
-		this.ub = domain.index(ub);
-	}
+        this.domain = variable.getDomain();
+        this.lb = domain.lowest(lb);
+        this.ub = domain.greatest(ub);
+    }
 
-	@Override
-	public int getEvaluation(final int reviseCount) {
-		return 0;
-	}
+    @Override
+    public int getEvaluation(final int reviseCount) {
+        return 0;
+    }
 
-	@Override
-	public boolean revise(final RevisionHandler revisator, final int reviseCount) {
-		final int lb;
-		if (this.lb < 0) {
-			lb = domain.first();
-		} else {
-			lb = this.lb;
-		}
-		final int ub;
-		if (this.ub < 0) {
-			ub = domain.last();
-		} else {
-			ub = this.ub;
-		}
-		boolean changed = false;
-		for (int i = lb; i <= ub; i = domain.next(i)) {
-			domain.remove(i);
-			changed = true;
-		}
-		if (domain.size() <= 0) {
-			return false;
-		}
-		if (changed) {
-			revisator.revised(this, getVariable(0));
-		}
-		entail();
-		return true;
-	}
+    @Override
+    public boolean revise(final RevisionHandler revisator, final int reviseCount) {
+        boolean changed = false;
+        for (int i = lb; i <= ub; i = domain.next(i)) {
+            if (domain.present(i)) {
+                domain.remove(i);
+                changed = true;
+            }
+        }
+        if (changed) {
+            if (domain.size() <= 0) {
+                return false;
+            }
+            revisator.revised(this, getVariable(0));
+        }
+        entail();
+        return true;
+    }
 
-	@Override
-	public boolean isConsistent(final int reviseCount) {
-		return (this.lb > 0 && domain.first() < lb)
-				|| (this.ub >= 0 && domain.last() > ub);
-	}
+    @Override
+    public boolean isConsistent(final int reviseCount) {
+        return (this.lb > 0 && domain.first() < lb)
+                || (this.ub >= 0 && domain.last() > ub);
+    }
 
-	@Override
-	public boolean check() {
-		final int value = getValue(0);
-		return value < lb || ub < value;
-	}
-	
-	@Override
-	public String toString() {
-		return getVariable(0) + " notin [" + lb + ", " + ub + "]";
-	}
+    @Override
+    public boolean check() {
+        final int value = getValue(0);
+        return value < lb || ub < value;
+    }
+
+    @Override
+    public String toString() {
+        return getVariable(0) + " notin [" + getVariable(0).getValue(lb) + ", "
+                + getVariable(0).getValue(ub) + "]";
+    }
 }
--- src/main/java/cspfj/generator/constraint/AllDifferentGenerator.java
+++ src/main/java/cspfj/generator/constraint/AllDifferentGenerator.java
@@ -20,70 +20,78 @@ import cspom.constraint.GeneralConstraint;
 
 public final class AllDifferentGenerator extends AbstractGenerator {
 
-	private static int allDiff = 0;
+    private static int allDiff = 0;
 
-	public AllDifferentGenerator(final Problem problem) {
-		super(problem);
-	}
+    public AllDifferentGenerator(final Problem problem) {
+        super(problem);
+    }
 
-	@Override
-	public boolean generate(final CSPOMConstraint constraint)
-			throws FailedGenerationException {
+    @Override
+    public boolean generate(final CSPOMConstraint constraint)
+            throws FailedGenerationException {
 
-		if (!(constraint instanceof GeneralConstraint)) {
-			throw new FailedGenerationException(constraint
-					+ " is not supported");
-		}
+        if (!(constraint instanceof GeneralConstraint)) {
+            throw new FailedGenerationException(constraint
+                    + " is not supported");
+        }
 
-		final Variable[] solverVariables = getSolverVariables(constraint
-				.getScope());
-		if (nullVariable(solverVariables) != null) {
-			return false;
-		}
+        final Variable[] solverVariables = getSolverVariables(constraint
+                .getScope());
+        if (nullVariable(solverVariables) != null) {
+            return false;
+        }
 
-		final int[] values = values(solverVariables);
+        final int[] values = values(solverVariables);
 
-		final Variable[][][] A = new Variable[solverVariables.length][values.length][values.length];
-		for (int i = solverVariables.length; --i >= 0;) {
-			for (int u = values.length; --u >= 0;) {
-				final int uV = values[u];
-				for (int l = u; --l >= 0 && l > u - solverVariables.length + 1;) {
-					final int lV = values[l];
-					A[i][l][u] = addVariable("A" + allDiff + "_" + i + "_" + l
-							+ "_" + u, new BooleanDomain());
+        final Variable[][][] A = new Variable[solverVariables.length][values.length][values.length];
+        for (int i = solverVariables.length; --i >= 0;) {
+            final Variable solverVariable = solverVariables[i];
+            for (int u = values.length; --u >= 0;) {
+                final int uV = values[u];
+                for (int l = u; --l >= 0 && l > u - solverVariables.length + 1;) {
 
-					addConstraint(new ReifiedConstraint(A[i][l][u],
-							new InInterval(solverVariables[i], lV, uV),
-							new NotInInterval(solverVariables[i], lV, uV)));
-				}
-			}
-		}
+                    final int lV = values[l];
+                    if (solverVariable.getDomain().lowest(lV) >= 0
+                            && solverVariable.getDomain().greatest(uV) >= 0) {
+                        A[i][l][u] = addVariable("A" + allDiff + "_" + i + "_"
+                                + l + "_" + u, new BooleanDomain());
 
-		for (int u = values.length; --u >= 0;) {
-			for (int l = u; --l >= 0 && l > u - solverVariables.length + 1;) {
-				final Collection<Variable> sum = new ArrayList<Variable>();
-				for (int i = solverVariables.length; --i >= 0;) {
-					sum.add(A[i][l][u]);
-				}
-				addConstraint(new SumLeq(u - l + 1, sum
-						.toArray(new Variable[sum.size()])));
-			}
+                        addConstraint(new ReifiedConstraint(A[i][l][u],
+                                new InInterval(solverVariable, lV, uV),
+                                new NotInInterval(solverVariable, lV, uV)));
+                    }
 
-		}
-		allDiff++;
-		return true;
-	}
+                }
+            }
+        }
 
-	private static int[] values(final Variable[] variables) {
-		final Set<Integer> valueSet = new HashSet<Integer>();
-		for (Variable v : variables) {
-			for (int i = v.getFirst(); i >= 0; i = v.getNext(i)) {
-				valueSet.add(v.getValue(i));
-			}
-		}
-		final int[] values = IntLinkedList.intCollectionToArray(valueSet);
-		Arrays.sort(values);
-		return values;
-	}
+        for (int u = values.length; --u >= 0;) {
+            for (int l = u; --l >= 0 && l > u - solverVariables.length + 1;) {
+                final Collection<Variable> sum = new ArrayList<Variable>();
+                for (int i = solverVariables.length; --i >= 0;) {
+                    if (A[i][l][u] != null) {
+                        sum.add(A[i][l][u]);
+                    }
+                }
+                addConstraint(new SumLeq(u - l + 1, sum
+                        .toArray(new Variable[sum.size()])));
+            }
+
+        }
+        allDiff++;
+        return true;
+    }
+
+    private static int[] values(final Variable[] variables) {
+        final Set<Integer> valueSet = new HashSet<Integer>();
+        for (Variable v : variables) {
+            for (int i = v.getFirst(); i >= 0; i = v.getNext(i)) {
+                valueSet.add(v.getValue(i));
+            }
+        }
+        final int[] values = IntLinkedList.intCollectionToArray(valueSet);
+        Arrays.sort(values);
+        return values;
+    }
 
 }
