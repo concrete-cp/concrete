539f7992d2c70fa5960abc16b0fccf1517f262d8
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2010-07-12 11:10:59 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspfj@535 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/java/cspfj/constraint/extension/TupleSet.java
+++ src/main/java/cspfj/constraint/extension/TupleSet.java
@@ -1,70 +1,67 @@
 package cspfj.constraint.extension;
 
-import java.math.BigInteger;
 import java.util.Iterator;
 
 import cspfj.util.IntTupleSet;
 
-public class TupleSet implements Matrix, Cloneable, Iterable<int[]> {
+public final class TupleSet implements Matrix, Cloneable, Iterable<int[]> {
 
-    private IntTupleSet tupleSet;
+	private IntTupleSet tupleSet;
 
-    private final boolean initialContent;
+	private final boolean initialContent;
 
-    private boolean empty;
+	private boolean empty;
 
-    public TupleSet(final int nbTuples, boolean initialContent) {
-        super();
-        tupleSet = new IntTupleSet(nbTuples);
-        this.initialContent = initialContent;
-        empty = !initialContent;
-    }
+	public TupleSet(final int nbTuples, final boolean initialContent) {
+		super();
+		tupleSet = new IntTupleSet(nbTuples);
+		this.initialContent = initialContent;
+		empty = !initialContent;
+	}
 
-    public TupleSet(boolean initialContent) {
-        this(16, initialContent);
-    }
+	public TupleSet(final boolean initialContent) {
+		this(16, initialContent);
+	}
 
-    public TupleSet clone() {
-        final TupleSet clone;
-        try {
-            clone = (TupleSet) super.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new InternalError(e.toString());
-        }
-        clone.tupleSet = new IntTupleSet(tupleSet);
-        return clone;
-    }
+	public TupleSet clone() {
+		final TupleSet clone;
+		try {
+			clone = (TupleSet) super.clone();
+		} catch (CloneNotSupportedException e) {
+			throw new InternalError(e.toString());
+		}
+		clone.tupleSet = new IntTupleSet(tupleSet);
+		return clone;
+	}
 
-    @Override
-    public boolean check(int[] tuple) {
-        return tupleSet.containsTuple(tuple) ^ initialContent;
-    }
+	@Override
+	public boolean check(final int[] tuple) {
+		return tupleSet.containsTuple(tuple) ^ initialContent;
+	}
 
-    @Override
-    public void set(int[] tuple, boolean status) {
-        if (status == initialContent) {
-            tupleSet.remove(tuple);
-        } else {
-            tupleSet.add(tuple.clone());
-        }
-        empty = false;
-    }
+	@Override
+	public void set(final int[] tuple, final boolean status) {
+		if (status == initialContent) {
+			tupleSet.remove(tuple);
+		} else {
+			tupleSet.add(tuple.clone());
+		}
+		empty = false;
+	}
 
+	public boolean getInitialContent() {
+		return initialContent;
+	}
 
+	public boolean isEmpty() {
+		return empty;
+	}
 
-    public boolean getInitialContent() {
-        return initialContent;
-    }
+	public int size() {
+		return tupleSet.size();
+	}
 
-    public boolean isEmpty() {
-        return empty;
-    }
-
-    public int size() {
-        return tupleSet.size();
-    }
-
-    public Iterator<int[]> iterator() {
-        return tupleSet.iterator();
-    }
+	public Iterator<int[]> iterator() {
+		return tupleSet.iterator();
+	}
 }
