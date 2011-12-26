ce457897a6d680a107b2dbff186c03ed29c11064
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2011-12-26 20:19:37 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspfj@755 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/scala/cspfj/constraint/semantic/AllDifferent.scala
+++ src/main/scala/cspfj/constraint/semantic/AllDifferent.scala
@@ -28,6 +28,8 @@ import cspfj.util.BitVector
 import scala.annotation.tailrec
 import cspfj.constraint.Residues
 import cspfj.util.Loggable
+import cspfj.constraint.SimpleRemovals
+import scala.collection.immutable.BitSet
 
 final class Node(val n: Int) {
   var list: List[Node] = Nil
@@ -35,7 +37,7 @@ final class Node(val n: Int) {
   override def toString = n.toString + " : " + list.map(_.n)
 }
 
-final class AllDifferent(scope: Variable*) extends AbstractConstraint(null, scope.toArray) with Loggable {
+final class AllDifferent(scope: Variable*) extends AbstractConstraint(null, scope.toArray) with SimpleRemovals with Loggable {
 
   val offset = scope map { _.dom.allValues.head } min
   val max = scope map { _.dom.allValues.last } max
@@ -49,79 +51,77 @@ final class AllDifferent(scope: Variable*) extends AbstractConstraint(null, scop
     }
   }
 
-  val s = new Node(-1)
+  class Node(val v: Variable) {
+    var d = BitSet.empty ++ (v.values map (_ - offset))
 
-  val t = new Node(-1)
+    var child: Node = null
+    var next: Node = null
+    var rank = 1
+    
+    if (d.size == 1) filter(d, Set(v))
 
-  val varNodes = (0 until arity) map (i => i -> new Node(i)) toMap
+    override def toString = tree(0)
 
-  val valNodes = (for (v <- scope; i <- v.values) yield i).distinct map (i => i -> new Node(i)) toMap
+    def tree(depth: Int): String =
+      (0 until depth).map(_ => "--").fold("")(_ + _) + v + " (" + rank + ")\n" +
+        (if (child != null) child.tree(depth + 1) else "") +
+        (if (next != null) next.tree(depth) else "")
 
-  def init(pos: Int, idx: Int) {
-
-    s.list = varNodes.iterator.filter(_._1 != pos).map(_._2).toList
-
-    val value = scope(pos).dom.value(idx)
-    tuple(pos) = idx
-
-    fine("Matching for (" + pos + ", " + value + ")")
-
-    for (v <- valNodes.values) v.list = List(t)
-
-    for (p <- 0 until arity if p != pos) {
-      varNodes(p).list = Nil
-      for (v <- scope(p).values if v != value) {
-        varNodes(p).list ::= valNodes(v)
-        valNodes(v).list ::= varNodes(p)
-      }
+    def flatten: Set[Variable] = {
+      var s = Set(v)
+      if (child != null) s ++= child.flatten
+      if (next != null) s ++= next.flatten
+      s
     }
-
   }
 
-  @tailrec
-  def assign(s: List[Node], m: Int): Int =
-    if (s.tail.isEmpty) {
-      m
-    } else {
-      val value = s.head.n
-      val variable = s.tail.head.n
-      tuple(variable) = scope(variable).dom.index(value)
-      fine("matched (" + variable + ", " + value + ")")
-      assign(s.tail.tail, m + 1)
-    }
+  var root: Node = null
 
-  def matching(pos: Int, idx: Int) = {
+  def add(root: Node, n: Node): Node = {
+    if (root == null) n
+    else {
+      val common = root.d & n.d
 
-    init(pos, idx)
-
-    @tailrec
-    def alternate(stack: List[Node], b: Set[Node], matches: Int): Boolean = {
-      if (matches >= arity - 1) {
-        assert(matches == arity - 1)
-        true
-      } else if (stack.isEmpty) {
-        false
-      } else if (stack.head.list.isEmpty) {
-        alternate(stack.tail, b, matches)
-      } else {
-        val first = stack.head.list.head
-        stack.head.list = stack.head.list.tail
-
-        if (first == t) {
-          alternate(List(s), b, matches + assign(stack, 0))
-        } else if (b(first)) {
-          alternate(stack, b, matches)
-        } else {
-          alternate(first :: stack, b + first, matches)
+      if (root.d == common) {
+        n.child = root
+        n.rank = root.rank + 1
+        if (n.rank == n.d.size) {
+          filter(n.d, n.flatten)
         }
+        n
+      } else if (n.d == common) {
+        root.child = add(root.child, n)
+        root.rank += 1
+        if (root.rank == root.d.size) {
+          filter(root.d, root.flatten)
+        }
+        root
+      } else {
+        root.next = add(root.next, n)
+        root
       }
     }
-
-    alternate(List(s), Set.empty, 0)
-
   }
-  
-  
+
+  var change = false
+
+  def filter(values: Set[Int], except: Set[Variable]) {
+    for (v <- scope if !except(v)) {
+      for (i <- values.map(v.dom.index) if i >= 0) {
+        v.dom.remove(i)
+        change = true
+      }
+    }
+  }
+
+  def revise(rh: RevisionHandler, r: Int) = {
+    do {
+      change = false
+      root = scope.map(new Node(_)).reduce(add(_, _))
+    } while (change)
+
+    true
+  }
 
   override def toString = "allDifferent" + scope.mkString("(", ", ", ")")
 
