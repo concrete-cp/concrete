c88e60265def44f1223286879423f720506c7ad8
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2012-01-11 17:13:36 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/cspfj@757 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/scala/cspfj/constraint/semantic/AllDifferent.scala
+++ src/main/scala/cspfj/constraint/semantic/AllDifferent.scala
@@ -32,14 +32,13 @@ import scala.collection.immutable.BitSet
 import cspfj.util.Backtrackable
 import scala.util.Random
 import cspfj.util.Hasse
-import cspfj.util.UniversalSet
 import cspfj.util.SetInclusion
 
 final class AllDifferent(scope: Variable*)
   extends AbstractConstraint(null, scope.toArray)
   with Loggable
   with VariableGrainedRemovals
-  with Backtrackable[Hasse[(Variable, BitSet)]] {
+  with Backtrackable[Hasse[(Variable, Set[Int])]] {
 
   private val offset = scope map { _.dom.allValues.head } min
   private val max = scope map { _.dom.allValues.last } max
@@ -58,12 +57,17 @@ final class AllDifferent(scope: Variable*)
   private var tree: Hasse[(Variable, Set[Int])] = Hasse.empty(
     new PartialOrdering[(Variable, Set[Int])] {
       val si = new SetInclusion[Int]
+      def tryCompare(a: (Variable, Set[Int]), b: (Variable, Set[Int])) =
+        if (lteq(a, b)) {
+          if (lteq(b, a)) Some(0)
+          else Some(1)
+        } else None
       override def lteq(a: (Variable, Set[Int]), b: (Variable, Set[Int])) = si.lteq(a._2, b._2)
-    }, (null, UniversalSet), (null, Set.empty)) //scope.foldLeft(List[BDom]())((t, v) => add(v, dom(v), t)) //Nil
+    }) //scope.foldLeft(List[BDom]())((t, v) => add(v, dom(v), t)) //Nil
 
   def save() = tree
 
-  def restore(d: List[BDom[Variable]]) {
+  def restore(d: Hasse[(Variable, Set[Int])]) {
     tree = d
   }
 
@@ -77,126 +81,21 @@ final class AllDifferent(scope: Variable*)
     restoreLevel(l)
   }
 
-  private def add(v: Variable, dom: BitSet, tree: List[BDom[Variable]]): List[BDom[Variable]] = {
-    var subset = false
-
-    val nt = tree map { p =>
-      if (dom.subsetOf(p.dom)) {
-        subset = true
-        val child = add(v, dom, p.child)
-        BDom(v, dom, child, 1 + child.map(_.rank).sum)
-      } else if (p.dom.subsetOf(dom)) {
-        subset = true
-        BDom(v, dom, List(p), p.rank + 1)
-      } else p
-    }
-
-    if (subset) nt else BDom(v, dom, Nil, 1) :: nt
-    //    if (tree == Nil) BDom(v, dom, Nil, 1) :: Nil
-    //    else if (tree.head.dom.subsetOf(dom)) {
-    //
-    //      val (subsets, others) = tree.tail.partition(bd => bd.dom.subsetOf(dom))
-    //      BDom(v, dom, tree.head :: subsets, 1 + tree.head.rank) :: others
-    //
-    //    } else if (dom.subsetOf(tree.head.dom)) {
-    //
-    //      BDom(tree.head.v, tree.head.dom, add(v, dom, tree.head.child), 1 + tree.head.rank) :: tree.tail
-    //
-    //    } else tree.head :: add(v, dom, tree.tail)
-
-  }
-
-  private def clean(tree: List[BDom], variables: Set[Variable]): (List[BDom], Set[Variable]) = {
-    if (tree == Nil) (Nil, variables)
-    else if (variables.isEmpty) (tree, variables)
-    else if (variables.contains(tree.head.v)) {
-
-      val (child, remaining) = clean(tree.head.child, variables - tree.head.v)
-      val (tail, remaining2) = clean(tree.tail, remaining)
-      (child ::: tail, remaining2)
-
-    } else {
-
-      val (child, remaining) = clean(tree.head.child, variables)
-      val rank = 1 + child.map(_.rank).sum
-      val (tail, remaining2) = clean(tree.tail, remaining)
-      (BDom(tree.head.v, tree.head.dom, child, rank) :: tail, remaining2)
-
-    }
-  }
-
-  def toString(tree: List[BDom], depth: Int): String = {
-    if (tree == Nil) ""
-    else (0 until depth).map(_ => "--").reduce(_ + _) + tree.head.v + " (" + tree.head.rank + ")\n" +
-      toString(tree.head.child, depth + 1) +
-      toString(tree.tail, depth)
-
-  }
-
-  @tailrec
-  private def filter(tree: List[BDom], stack: List[BDom], modified: Set[Variable]): Set[Variable] = {
-    if (tree == Nil) {
-
-      if (stack == Nil) modified
-      else filter(stack, Nil, modified)
-
-    } else if (tree.head.rank >= tree.head.size) {
-
-      val preserve = flatten(tree.head.child) + tree.head.v
-      val changed = performFilter(preserve, tree.head.dom.toList)
-
-      changed.headOption match {
-        case Some(first) if first.dom.size == 0 => changed
-        case _ => filter(tree.head.child, stack ++ tree.tail, modified ++ changed)
-      }
-
-    } else filter(tree.head.child, stack ++ tree.tail, modified)
-
-  }
-
-  @tailrec
-  private def performFilter(v: Variable, values: List[Int], change: Boolean): Boolean = {
-    if (values == Nil) change
-    else {
-      val i = v.dom.index(values.head + offset)
-      if (i >= 0 && v.dom.present(i)) {
-        v.dom.remove(i)
-        if (v.dom.size == 0) true
-        else performFilter(v, values.tail, true)
-      } else performFilter(v, values.tail, change)
-    }
-  }
-
-  private def performFilter(preserve: Set[Variable], values: List[Int]): Set[Variable] = {
-    var changed: Set[Variable] = Set.empty
-    for (v <- scope) {
-      if (!preserve.contains(v) && performFilter(v, values, false)) {
-        if (v.dom.size == 0) return Set(v)
-        changed += v
-      }
-    }
-    changed
-  }
-
-  private def flatten(tree: List[BDom]): Set[Variable] = {
-    if (tree == Nil) Set.empty
-    else flatten(tree.head.child) ++ flatten(tree.tail) + tree.head.v
-  }
-
   def revise(rh: RevisionHandler, rvls: Int): Boolean = revise(rh, modified(rvls).toSet)
 
   private def revise(rh: RevisionHandler, changed: Set[Variable]): Boolean = {
     altering()
-    tree = clean(tree, changed)._1
-    tree = changed.foldLeft(tree)((t, v) => add(v, dom(v), t))
-    val vars = filter(tree, Nil, Set.empty)
-
-    if (vars.isEmpty) true
-    else if (vars.exists(_.dom.size == 0)) false
-    else {
-      vars.foreach(rh.revised(this, _))
-      revise(rh, vars)
-    }
+    true
+    //    tree = clean(tree, changed)._1
+    //    tree = changed.foldLeft(tree)((t, v) => add(v, dom(v), t))
+    //    val vars = filter(tree, Nil, Set.empty)
+    //
+    //    if (vars.isEmpty) true
+    //    else if (vars.exists(_.dom.size == 0)) false
+    //    else {
+    //      vars.foreach(rh.revised(this, _))
+    //      revise(rh, vars)
+    //    }
   }
 
   override def toString = "allDifferent" + scope.mkString("(", ", ", ")")
--- src/main/scala/cspfj/util/Hasse.scala
+++ src/main/scala/cspfj/util/Hasse.scala
@@ -1,59 +1,116 @@
 package cspfj.util
-import cspfj.constraint.semantic.BDom
+
 import scala.collection.immutable.BitSet
 import scala.util.Random
 
 case class HNode[A](
   val v: A,
-  val rank: Int,
-  val child: List[HNode[A]])
-
-object Hasse {
-  def empty[A](o: PartialOrdering[A], u: A, e: A) = new Hasse[A](o, HNode(u, 1, List(HNode(e, 0, Nil))))
+  val child: List[HNode[A]]) {
+  val rank: Int = if (child == Nil) 0 else 1 + child.map(_.rank).sum
 }
 
-class Hasse[A](val o: PartialOrdering[A], val root: HNode[A]) {
+object Hasse {
+  def empty[A](po: PartialOrdering[A]) = new Hasse[A](po, Nil)
+}
 
-  def +(v: A): Hasse[A] = new Hasse[A](o, add(v, root))
+class Hasse[A](val po: PartialOrdering[A], val roots: List[HNode[A]]) {
 
-  private def add(v: A, root: HNode[A]): HNode[A] = {
-    //var subset = false
+  def +(v: A): Hasse[A] = new Hasse[A](po, add(v, roots))
 
-    if (o.lteq(v, root.v)) {
-      HNode(root.v, root.rank + 1, root.child.map(add(v, _)))
-    } else if (o.lteq(root.v, v)) {
-      HNode(v, 1 + root.rank, List(root))
-    } else root
+  private def add(v: A, roots: List[HNode[A]]): List[HNode[A]] = {
+
+    val (subsets, remaining) = roots.partition(r => po.lteq(r.v, v))
+
+    if (subsets.isEmpty) {
+
+      val (supersets, remaining) = roots.partition(r => po.lteq(v, r.v))
+      if (supersets.isEmpty) {
+        HNode(v, collect(v, remaining, Set.empty).toList) :: remaining
+      } else {
+        remaining ::: supersets map { r => HNode(r.v, add(v, r.child)) }
+      }
+      
+    } else {
+      HNode(v, subsets ::: collect(v, remaining, Set.empty).toList) :: remaining
+    }
 
   }
 
-  override def toString: String = root.toString
+  private def collect(v: A, roots: List[HNode[A]], collected: Set[HNode[A]]): Set[HNode[A]] = {
+    if (roots == Nil) collected
+    else {
+      val (subsets, remaining) = roots.partition(r => po.lteq(r.v, v))
+      collect(v, remaining.flatMap(r => r.child.filter(!collected(_))), collected ++ subsets)
+    }
+  }
+
+  override def toString: String = roots.toString
+
+  def toGML = {
+    val stb = new StringBuilder();
+    stb.append("graph [\n");
+    stb.append("directed 0\n");
+
+    def flatnodes(r: List[HNode[A]]): Set[A] = r.map(_.v).toSet ++ r.flatMap(v => flatnodes(v.child))
+
+    for (v <- flatnodes(roots)) {
+      stb.append("node [\n");
+      stb.append("id \"").append(v.toString).append("\"\n");
+      stb.append("label \"").append(v.toString).append("\"\n");
+      stb.append("]\n");
+    }
+
+    def edges(s: HNode[A], r: List[HNode[A]]): String = (if (s != null) r.map { n =>
+      val stb = new StringBuilder
+      stb.append("edge [\n");
+      stb.append("source \"").append(s.v).append("\"\n");
+      stb.append("target \"").append(n.v.toString).append("\"\n");
+      stb.append("]\n");
+      stb.toString
+    }.foldLeft("")(_ + _)
+    else "") + (r.flatMap(n => edges(n, n.child)).foldLeft("")(_ + _))
+
+    stb.append(edges(null, roots))
+    stb.append("]\n").toString
+  }
 
 }
 
-class SetInclusion[A] extends PartialOrdering[Set[A]] {
-  def ltEq(a: Set[A], b: Set[A]) =
-    if (b == UniversalSet) true else a.subsetOf(b)
+trait PredefPO[A] extends PartialOrdering[A] {
+  def tryCompare(a: A, b: A) =
+    if (lteq(a, b)) {
+      if (lteq(b, a)) Some(0)
+      else Some(-1)
+    } else None
 }
 
-object UniversalSet extends BitSet
+class SetInclusion[A] extends PredefPO[Set[A]] {
+  def lteq(a: Set[A], b: Set[A]) = a.subsetOf(b)
+}
 
 object Test {
-  val rand = new Random
+  val rand = new Random(0)
 
   def randSet() = {
-    BitSet.empty ++ Stream.continually(rand.nextInt(10)).take(rand.nextInt(10))
+    BitSet.empty ++ Stream.continually(rand.nextInt(5)).take(rand.nextInt(5))
   }
 
   def main(a: Array[String]) {
-    var h: Hasse[(Int, BitSet)] = Hasse.empty(new PartialOrdering[(Int, BitSet)] {
+
+    var h: Hasse[(Int, Set[Int])] = Hasse.empty(new PredefPO[(Int, Set[Int])] {
       val si = new SetInclusion[Int]
-      override def ltEq(a: (Int, BitSet), b: (Int, BitSet)) = si.ltEq(a._2, b._2)
-    }, (0, UniversalSet), (0, BitSet.empty))
+
+      override def lteq(a: (Int, Set[Int]), b: (Int, Set[Int])) = si.lteq(a._2, b._2)
+    })
+
     var i = 0
-    while (true) {
-      h += (i, randSet())
+    while (i < 10) {
+      val s = randSet()
+      h += (i, s)
+      println((i, s))
       i += 1
     }
+    println(h)
+    println(h.toGML)
   }
 }
