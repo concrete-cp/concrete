aa4aaf9029c100427569837a52cf93ab2e48f6a5
Julien Vion <julien.vion@univ-valenciennes.fr>
2013-12-04 11:05:15 +0100

    Nouvelle heuristique de choix de valeurs aléatoire

---
+++ src/main/scala/concrete/heuristic/RandomValue.scala
@@ -0,0 +1,38 @@
+package concrete.heuristic;
+
+import concrete.Variable
+import concrete.Problem
+import scala.util.Random
+
+final class RandomValue extends ValueHeuristic {
+
+  def score(variable: Variable, index: Int) = -index
+
+  override def toString = "lexico";
+
+  def compute(p: Problem) {
+    // Nothing to compute
+  }
+
+  private val rand = new Random(0)
+
+  @annotation.tailrec
+  private def select(list: Iterator[Int], best: Int, ties: Int): Int = {
+    if (list.isEmpty) { best }
+    else {
+      val current = list.next
+
+      if (rand.nextDouble() * ties < 1) {
+        select(list, current, ties + 1)
+      } else {
+        select(list, best, ties + 1)
+      }
+    }
+  }
+
+  override def selectIndex(variable: Variable) = {
+    val itr = variable.dom.indicesIterator
+    select(itr, itr.next(), 2)
+  }
+
+}
