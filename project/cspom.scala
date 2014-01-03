aba339efdc349a231d684d475c9f58a12cad8bda
Julien Vion <julien.vion@univ-valenciennes.fr>
2014-01-03 14:52:13 +0100

    CSPOM back to lowercase, wasn't working anyway.

--- project/cspom.scala
+++ project/cspom.scala
@@ -2,8 +2,7 @@ import sbt._
 import Keys._
 
 object cspom extends Build {
-
   lazy val root = Project(id = "concrete", base = file(".")) dependsOn (cspom)
 
-  lazy val cspom = RootProject(file("../CSPOM"))
+  lazy val cspom = RootProject(file("../cspom"))
 }
