c963e1ff8c714b4329a5c7d3e0b0916f62db5738
Julien Vion <julien.vion@univ-valenciennes.fr>
2014-01-05 00:10:18 +0100

    Simplified code making use of value-aware methods in Domain. Might be a bit slower due to additional Function references.

--- src/main/scala/concrete/constraint/semantic/Eq.scala
+++ src/main/scala/concrete/constraint/semantic/Eq.scala
@@ -36,18 +36,25 @@ final class Eq(val neg: Boolean, val x: Variable, val b: Int, val y: Variable)
 
   def checkValues(t: Array[Int]) = (if (neg) -t(0) else t(0)) + b == t(1);
 
-  def yIndex(xIndex: Int) =
-    if (neg) y.dom.index(-x.dom.value(xIndex) + b)
-    else y.dom.index(x.dom.value(xIndex) + b)
+  private def yValue(x: Int) =
+    if (neg) -x + b else x + b
 
-  def xIndex(yIndex: Int) = {
-    val yv = y.dom.value(yIndex)
-    if (neg) x.dom.index(b - yv) else x.dom.index(yv - b)
-  }
+  private def xValue(y: Int) =
+    if (neg) b - y else y - b
+  //
+  //  def yIndex(xIndex: Int) =
+  //    if (neg) y.dom.index(-x.dom.value(xIndex) + b)
+  //    else y.dom.index(x.dom.value(xIndex) + b)
+  //
+  //  def xIndex(yIndex: Int) = {
+  //    val yv = y.dom.value(yIndex)
+  //    if (neg) x.dom.index(b - yv) else x.dom.index(yv - b)
+  //  }
 
   def shave(): List[Int] = {
     var mod: List[Int] = Nil
     if (neg) {
+      // -x + b = y <=> x = -y + b 
       if (scope(0).dom.intersectVal(scope(1).dom.valueInterval.negate + b)) {
         mod ::= 0
       }
@@ -55,6 +62,7 @@ final class Eq(val neg: Boolean, val x: Variable, val b: Int, val y: Variable)
         mod ::= 1
       }
     } else {
+      // x + b = y <=> x = y - b
       if (scope(0).dom.intersectVal(scope(1).dom.valueInterval - b)) {
         mod ::= 0
       }
@@ -66,25 +74,20 @@ final class Eq(val neg: Boolean, val x: Variable, val b: Int, val y: Variable)
   }
 
   def reviseVariable(position: Int, mod: List[Int]) = position match {
-    case 0 => x.dom.filter { i =>
-      val index = yIndex(i)
-      index >= 0 && y.dom.present(index)
+    case 0 => x.dom.filterValues { xv =>
+      y.dom.presentVal(yValue(xv))
     }
 
-    case 1 => y.dom.filter { i =>
-      val index = xIndex(i)
-      index >= 0 && x.dom.present(index)
+    case 1 => y.dom.filterValues { yv =>
+      x.dom.presentVal(xValue(yv))
     }
 
     case _ => throw new IllegalArgumentException
   }
 
   override def isConsistent() = {
-    val otherDom = y.dom
-
-    x.dom.indices.exists { i =>
-      val index = yIndex(i)
-      index >= 0 && otherDom.present(index)
+    x.dom.values.exists { xv =>
+      y.dom.presentVal(yValue(xv))
     }
   }
 
