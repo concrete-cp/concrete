5c753f53daf33c3608a84617ad47673e6b1a809d
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-03-17 17:30:51 +0100

    Added support for reified boolean equality

--- src/main/scala/concrete/generator/cspompatterns/BoolEq.scala
+++ src/main/scala/concrete/generator/cspompatterns/BoolEq.scala
@@ -15,31 +15,36 @@ import cspom.variable.IntExpression.implicits.arithmetics
 import cspom.variable.IntExpression
 import cspom.variable.IntExpression.implicits.ranges
 import cspom.variable.SimpleExpression
+import cspom.variable.BoolExpression
 
 object BoolEq extends ConstraintCompiler {
 
-  type A = (SimpleExpression[_], SimpleExpression[_])
+  type A = (SimpleExpression[_], SimpleExpression[_], SimpleExpression[_])
 
   override def constraintMatcher = {
     case CSPOMConstraint(
-      CSPOMConstant(true), 'eq,
+      r: SimpleExpression[_], 'eq,
       Seq(a: SimpleExpression[_], b: SimpleExpression[_]), _) if (
       a.isInstanceOf[BoolVariable] || b.isInstanceOf[BoolVariable]) =>
-      (a, b)
+      (r, a, b)
   }
 
   def compile(c: CSPOMConstraint[_], problem: CSPOM, data: A) = {
 
-    val (a, b) = data
+    val (r, a, b) = data
 
-    val na = BoolVariable.boolExpression(a)
-    val nb = BoolVariable.boolExpression(b)
+    val nr = BoolExpression(r)
+    val na = BoolExpression(a)
+    val nb = BoolExpression(b)
 
     replace(Seq(a), na, problem) ++
       replace(Seq(b), nb, problem) ++
+      replace(Seq(r), nr, problem) ++
       replaceCtr(c, Seq(
-        CSPOMDriver.clause(Seq(nb), Seq(na)),
-        CSPOMDriver.clause(Seq(na), Seq(nb))), problem)
+        CSPOMDriver.clause(Seq(nb), Seq(nr, na)),
+        CSPOMDriver.clause(Seq(na), Seq(nr, na)),
+        CSPOMDriver.clause(Seq(nr, na, nb), Seq()),
+        CSPOMDriver.clause(Seq(nr), Seq(na, nb))), problem)
 
   }
 
