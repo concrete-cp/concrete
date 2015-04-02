3c4fde26860d9d8d03230fedd040506843e5abdc
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-04-02 18:06:28 +0200

    Some simplification in GtGenerator

--- src/main/scala/concrete/generator/constraint/GtGenerator.scala
+++ src/main/scala/concrete/generator/constraint/GtGenerator.scala
@@ -15,46 +15,33 @@ final object GtGenerator extends Generator {
   override def gen(constraint: CSPOMConstraint[Boolean])(implicit variables: VarMap) = {
     val Seq(v0, v1) = constraint.arguments map cspom2concrete1D;
 
-    constraint.function match {
-      case 'gt => gte(v0, v1, true)
-      case 'ge => gte(v0, v1, false)
+    val strict = constraint.function match {
+      case 'gt => true
+      case 'ge => false
       case _   => throw new FailedGenerationException("Unhandled constraint " + constraint);
     }
-
-  }
-
-//  override def genReversed(constraint: CSPOMConstraint[Boolean])(implicit variables: VarMap) = {
-//    val Seq(v0, v1) = constraint.arguments map cspom2concrete1D;
-//
-//    constraint.function match {
-//      case 'gt => gte(v1, v0, false)
-//      case 'ge => gte(v1, v0, true)
-//      case _   => throw new FailedGenerationException("Unhandled constraint " + constraint);
-//    }
-//
-//  }
-
-  private def gte(v0: C21D, v1: C21D, strict: Boolean): Seq[Constraint] = (v0, v1) match {
-    case (Const(v0), Const(v1)) =>
-      require(v0 > v1 || (!strict && v0 >= v1))
-      Nil
-    case (Var(v0), Const(v1)) =>
-      if (strict) {
-        require(v0.initDomain.head > v1)
-      } else {
-        require(v0.initDomain.head >= v1)
-      }
-      Nil
-    case (Const(v0), Var(v1)) =>
-      if (strict) {
-        require(v0 > v1.initDomain.last)
-      } else {
-        require(v0 >= v1.initDomain.last)
-      }
-      Nil
-    case (Var(v0), Var(v1)) =>
-      Seq(
-        new Gt(v0, v1, strict))
+    (v0, v1) match {
+      case (Const(v0), Const(v1)) =>
+        require(v0 > v1 || (!strict && v0 >= v1))
+        Nil
+      case (Var(v0), Const(v1)) =>
+        if (strict) {
+          require(v0.initDomain.head > v1)
+        } else {
+          require(v0.initDomain.head >= v1)
+        }
+        Nil
+      case (Const(v0), Var(v1)) =>
+        if (strict) {
+          require(v0 > v1.initDomain.last)
+        } else {
+          require(v0 >= v1.initDomain.last)
+        }
+        Nil
+      case (Var(v0), Var(v1)) =>
+        Seq(
+          new Gt(v0, v1, strict))
+    }
   }
 
   override def genFunctional(constraint: CSPOMConstraint[_], r: C2Conc)(implicit variables: VarMap) = {
@@ -71,8 +58,7 @@ final object GtGenerator extends Generator {
 
     (v0, v1) match {
       case (Const(v0), Const(v1)) =>
-        val r = v0 > v1 || (!strict && v0 >= v1)
-        require(result.initDomain == BooleanDomain(r), constraint)
+        require(result.initDomain == BooleanDomain(v0 > v1 || (!strict && v0 >= v1)), constraint)
         Nil
       case (Var(v0), Const(v1)) =>
         Seq(new ReifiedConstraint(
