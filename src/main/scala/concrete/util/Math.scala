f0ff7ccd885cb516009857a21878ade92510c4cd
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-07-08 17:45:38 +0200

    Moved GCD calculations to Math.util

--- src/main/scala/concrete/util/Math.scala
+++ src/main/scala/concrete/util/Math.scala
@@ -22,4 +22,33 @@ object Math {
     if (result != result.toInt) throw new ArithmeticException("overflow")
     result.toInt;
   }
+
+  /** Make sure not to divide by some negative number as it would reverse the inequality **/
+  def gcd(a: Seq[Int]): BigInt = a.map(BigInt(_)).reduce(_.gcd(_))
+
+  def gcd(ia: Int, ib: Int): Int = {
+    var d = 0
+    var a = ia
+    var b = ib
+    while (even(a) && even(b)) {
+      a /= 2
+      b /= 2
+      d += 1
+    }
+    while (a != b) {
+      if (even(a)) {
+        a /= 2
+      } else if (even(b)) {
+        b /= 2
+      } else if (a > b) {
+        a = (a - b) / 2
+      } else {
+        b = (b - a) / 2
+      }
+    }
+
+    a * (0x1 << d)
+  }
+
+  def even(a: Int) = (a & 0x1) == 0
 }
