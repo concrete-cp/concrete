19f22b3596748891221534616fea4f89c53150ae
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-07-10 18:12:05 +0200

    Added tests for Interval.intersection

--- src/test/scala/concrete/util/IntervalTest.scala
+++ src/test/scala/concrete/util/IntervalTest.scala
@@ -17,4 +17,13 @@ class IntervalTest extends FlatSpec with Matchers {
     (Interval(1, 20) / -20) shouldBe Interval(-1, -1)
     (Interval(0, 20) / -20) shouldBe Interval(-1, 0)
   }
+
+  it should "detect intersection" in {
+    assert(!(Interval(0, 0) intersects Interval(3, 4)))
+    assert(!(Interval(0, 0) intersects Interval(-4, -3)))
+    assert(Interval(1, 5) intersects Interval(2, 7))
+    assert(Interval(1, 5) intersects Interval(5, 6))
+    assert(Interval(1, 5) intersects Interval(2, 4))
+    assert(Interval(-5, 3) intersects Interval(1, 4))
+  }
 }
