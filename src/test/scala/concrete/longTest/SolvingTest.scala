4bf6e925d89d73d32edfa834003f85ded5def261
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-07-17 17:45:51 +0200

    SolvingTest now reads search annotations on FlatZinc files, reinits ParameterManager before each search, uses Futures for timeouts (does not really work…) and tests optimization problems.

--- src/test/scala/concrete/longTest/SolvingTest.scala
+++ src/test/scala/concrete/longTest/SolvingTest.scala
@@ -21,7 +21,9 @@ import cspom.xcsp.XCSPParser
 import cspom.flatzinc.FlatZincParser
 import concrete.runner.FZConcrete
 import cspom.flatzinc.FZSolve
-
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
 //import SolvingTest._
 
 class SolvingTest extends FlatSpec with SolvingBehaviors {
@@ -34,6 +36,8 @@ class SolvingTest extends FlatSpec with SolvingBehaviors {
 trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this: FlatSpec =>
 
   val problemBank = LinkedHashMap[String, AnyVal](
+    "1d_rubiks_cube.fzn" -> 12,
+    "scen11-f12.xml.bz2" -> 0,
     "crossword-m1-debug-05-01.xml" -> 48,
     "bigleq-50.xml" -> 1,
     "battleships_2.fzn" -> 36,
@@ -49,7 +53,6 @@ trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this:
     "crossword-m2-debug-05-01.xml" -> 48,
     "queens-8.xml" -> 92,
 
-    "scen11-f12.xml.bz2" -> 0,
     "scen11.xml.bz2" -> true,
     "bqwh-15-106-0_ext.xml" -> 182,
     "queensAllDiff-8.xml.bz2" -> 92,
@@ -62,11 +65,12 @@ trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this:
     "tsp-20-1_ext.xml.bz2" -> true,
 
     "test.fzn" -> true,
-    "1d_rubiks_cube.fzn" -> true,
+
     "queens-12.xml" -> 14200)
 
-  def test(parameters: ParameterManager = new ParameterManager()): Unit = {
+  def test(parameters: Seq[(String, String)] = Nil): Unit = {
     for ((p, r) <- problemBank) {
+
       val expected: Boolean = r match {
         case e: Int     => e > 0
         case b: Boolean => b
@@ -86,7 +90,10 @@ trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this:
     }
   }
 
-  def solve(name: String, expectedResult: Boolean, parameters: ParameterManager): Unit = {
+  def solve(name: String, expectedResult: Boolean, parameters: Seq[(String, String)]): Unit = {
+
+    val pm = new ParameterManager
+    parameters.foreach(s => pm.update(s._1, s._2))
 
     val url = getClass.getResource(name)
 
@@ -103,7 +110,7 @@ trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this:
             CSPOMCompiler.compile(cspomProblem, FZPatterns()).get
             goal = data.get('goal).collect {
               case g: FZSolve =>
-                FZConcrete.parseGoal(g, false, parameters)
+                FZConcrete.parseGoal(g, false, pm)
                 g
             }
             false
@@ -116,7 +123,7 @@ trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this:
 
         //logger.info(cspomProblem.toString)
 
-        Solver(cspomProblem, parameters).map { solver =>
+        Solver(cspomProblem, pm).map { solver =>
 
           for (g <- goal) {
             FZConcrete.parseSearchMode(g, solver, false)
@@ -124,41 +131,55 @@ trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this:
 
           //    println(solver.concreteProblem)
 
-          val solvIt = solver.toIterable
+          val f = Future {
 
-          if (expectedResult) {
-            solvIt should not be 'empty
-          } else {
-            solvIt shouldBe 'empty
-          }
+            val solvIt = solver.toIterable
 
-          // println(solver.concreteProblem.toString)
+            if (expectedResult) {
+              solvIt should not be 'empty
+            } else {
+              solvIt shouldBe 'empty
+            }
 
-          if (test) {
-            for (sol <- solvIt.headOption) {
-              val failed = XCSPConcrete.controlCSPOM(sol, data('variables).asInstanceOf[Seq[String]], url)
-              withClue(sol) {
-                failed shouldBe 'empty
+            // println(solver.concreteProblem.toString)
+
+            if (test) {
+              for (sol <- solvIt.headOption) {
+                val failed = XCSPConcrete.controlCSPOM(sol, data('variables).asInstanceOf[Seq[String]], url)
+                withClue(sol) {
+                  failed shouldBe 'empty
+                }
               }
             }
           }
+
+          concurrent.Await.result(f, 1000.seconds)
         }
     }
       .get
   }
 
-  def count(name: String, expectedResult: Int, parameters: ParameterManager = new ParameterManager(),
+  def count(name: String, expectedResult: Int, parameters: Seq[(String, String)],
             test: Boolean): Unit = {
     val url = getClass.getResource(name)
 
     require(url != null, "Could not find resource " + name)
     val parser = CSPOM.autoParser(url).get
 
+    var goal: Option[FZSolve] = None
+
+    val pm = new ParameterManager
+
     CSPOM.load(url, parser).flatMap {
       case (cspomProblem, data) =>
         val test = parser match {
           case FlatZincParser =>
             CSPOMCompiler.compile(cspomProblem, FZPatterns()).get
+            goal = data.get('goal).collect {
+              case g: FZSolve =>
+                FZConcrete.parseGoal(g, false, pm)
+                g
+            }
             false
           case XCSPParser =>
             CSPOMCompiler.compile(cspomProblem, XCSPPatterns()).get
@@ -169,20 +190,37 @@ trait SolvingBehaviors extends Matchers with Inspectors with LazyLogging { this:
 
         logger.info(cspomProblem.toString)
 
-        Solver(cspomProblem, parameters).map { solver =>
+        parameters.foreach(s => pm.update(s._1, s._2))
 
-          val sols = solver.toStream.take(expectedResult + 1)
+        Solver(cspomProblem, pm).map { solver =>
 
-          if (test) {
-            forAll(sols.take(100)) { sol =>
-              val failed = XCSPConcrete.controlCSPOM(sol, data('variables).asInstanceOf[Seq[String]], url)
-              failed shouldBe 'empty
-            }
+          for (g <- goal) {
+            FZConcrete.parseSearchMode(g, solver, false)
           }
 
-          sols should have size expectedResult
+          val f = Future {
+            solver.optimizes match {
+              case Some(v) =>
+                val opt = solver.toIterable.last
+                opt.get(v).get shouldBe expectedResult
 
+              case None =>
+                val sols = solver.toStream.take(expectedResult + 1)
+
+                if (test) {
+                  forAll(sols.take(100)) { sol =>
+                    val failed = XCSPConcrete.controlCSPOM(sol, data('variables).asInstanceOf[Seq[String]], url)
+                    failed shouldBe 'empty
+                  }
+                }
+
+                sols should have size expectedResult
+            }
+
+          }
+          concurrent.Await.result(f, 1000.seconds)
         }
+
     } should be a 'success
 
   }
