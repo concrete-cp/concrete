de29965e0a1c30725dce8e8fe21f92080e895a6f
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-07-29 23:57:28 +0200

    Removed ElementBC. Now use the Domain union/intersection API for fast ElementAC.

--- src/main/scala/concrete/constraint/semantic/Element.scala
+++ src/main/scala/concrete/constraint/semantic/Element.scala
@@ -26,17 +26,18 @@ object Element {
     }
 
     Seq(
-      new ElementBC(result, index, vars),
-      new ElementAC(result, index, vars))
+      new Element(result, index, vars))
   }
 }
 
-abstract class Element(val result: Variable,
-                       val index: Variable,
-                       val vars: Array[Variable])
-  extends Constraint(result +: index +: vars.filter(_ ne null)) {
+class Element(val result: Variable,
+              val index: Variable,
+              val vars: Array[Variable])
+    extends Constraint(result +: index +: vars.filter(_ ne null)) with Removals {
 
   protected val varsOption = vars.map(Option.apply)
+
+  private var card: Int = _
 
   lazy val map: Map[Int, Int] = {
     val scopeIndices = scope.zipWithIndex.toMap
@@ -57,67 +58,15 @@ abstract class Element(val result: Variable,
       }
     }"
   }
-}
-
-class ElementBC(result: Variable, index: Variable, vars: Array[Variable])
-  extends Element(result, index, vars)
-  with BC {
-
-  def advise(ps: ProblemState, pos: Int): Int = arity
-
-  def shave(ps: ProblemState) = {
-    var ch = List[Int]()
-
-    val resultSpan = ps.span(result)
-    /**
-     * Revise indices
-     */
-    ps.filterDom(index) {
-      i: Int =>
-
-        i >= 0 && i < vars.length && varsOption(i).forall { v =>
-          resultSpan intersects ps.span(v)
-        }
-
-    }
-      .andThen { ps =>
-        /**
-         * Revise result
-         */
-        val union = ps.dom(index).iterator.map(v => ps.span(vars(v))).reduce(_ span _)
-        ps.shaveDom(result, union)
-      }
-      .andThen { ps =>
-        /**
-         * Revise vars
-         */
-        val index = ps.dom(this.index)
-        if (index.size == 1) {
-          ps.shaveDom(vars(index.singleValue), ps.span(this.result))
-        } else {
-          ps
-        }
-      }
-
-  }
-  def simpleEvaluation: Int = 2
-  override def toString(ps: ProblemState) = toString(ps, "BC")
-}
-
-class ElementAC(
-  result: Variable,
-  index: Variable,
-  vars: Array[Variable])
-  extends Element(result, index, vars) with Removals with BCCompanion {
-
-  val offset = vars.filter(_ ne null).map(_.initDomain.head).min
 
   def getEvaluation(ps: ProblemState): Int = {
-    val scopeSize = this.scopeSize(ps)
-    if (skip(ps, scopeSize)) -1 else scopeSize
+    card * ps.dom(index).size
   }
 
-  def skipIntervals = false
+  override def init(ps: ProblemState) = {
+    card = varsOption.flatten.map(ps.dom).map(_.size).max
+    ps.shaveDom(index, 0, vars.length)
+  }
 
   def revise(ps: ProblemState, modified: List[Int]): Outcome = {
     val resultDom = ps.dom(result)
@@ -125,33 +74,34 @@ class ElementAC(
      * Revise indices
      */
     ps.filterDom(this.index) { i =>
-      i >= 0 && i < vars.length && varsOption(i).forall { v =>
-        val d = ps.dom(v)
-        resultDom.exists(d.present)
-      }
-
+      varsOption(i).exists { v => (resultDom & ps.dom(v)).nonEmpty }
     }
       .andThen { ps =>
-        /**
-         * Revise result
-         */
-        var union = BitVector.empty
-        for (i <- ps.dom(index)) {
-          union |= ps.dom(vars(i)).bitVector(offset)
-        }
-
-        ps.filterDom(result)(v => union(v - offset))
-      }
-      .andThen { ps =>
-        /**
-         * Revise vars
-         */
         val index = ps.dom(this.index)
-        if (index.size == 1) {
-          ps.filterDom(vars(index.singleValue))(ps.dom(result).present)
-        } else ps
-      }
 
+        if (index.size == 1) {
+          val selectedVar = vars(index.singleValue)
+          //println(selectedVar.toString(ps))
+          val intersect = ps.dom(selectedVar) & resultDom
+          ps.updateDom(result, intersect)
+            .updateDom(selectedVar, intersect)
+        } else {
+
+          /**
+           * Revise result
+           */
+ 
+          val it = index.iterator
+          var union = ps.dom(vars(it.next()))
+          while (it.hasNext) {
+            union |= ps.dom(vars(it.next()))
+          }
+          //.map(i => ps.dom(vars(i))).reduce(_ | _) //reduceLeft((union, i) => union | ps.dom(vars(i)))
+
+          ps.updateDom(result, resultDom & union)
+
+        }
+      }
   }
   def simpleEvaluation: Int = 3
 
