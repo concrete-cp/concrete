1d7ed41260d9d35ce942e71cc2533f503592258f
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-07-30 00:02:34 +0200

    OccurrenceDomains simplified

--- src/main/scala/concrete/generator/cspompatterns/OccurrenceDomains.scala
+++ src/main/scala/concrete/generator/cspompatterns/OccurrenceDomains.scala
@@ -9,20 +9,14 @@ import cspom.variable.SimpleExpression
 import cspom.util.IntInterval
 import cspom.variable.IntExpression
 
-
 /**
  * @author vion
  */
 object OccurrenceDomains extends VariableCompiler('occurrence) {
   def compiler(c: CSPOMConstraint[_]): Map[CSPOMExpression[_], CSPOMExpression[_]] = {
-    val CSPOMConstraint(r, _, Seq(const @ CSPOMConstant(value), CSPOMSeq(args)), _) = c
+    val CSPOMConstraint(r, _, Seq(_, CSPOMSeq(args)), _) = c
     val ir = IntExpression.coerce(r)
-    val lb = args.count(_ == const)
-    val ub = args.count {
-      case s: SimpleExpression[_] => s.contains(value)
-      case _ => throw new IllegalArgumentException("Occurrence constraint only supports SimpleExpressions")
-    }
 
-    Map(r -> reduceDomain(ir, IntInterval(lb, ub)))
+    Map(r -> reduceDomain(ir, IntInterval(0, args.size)))
   }
 }
