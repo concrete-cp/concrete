8e5a4ec68080b450ac55387ef604741ae0e83844
Julien Vion <julien.vion@univ-valenciennes.fr>
2015-09-24 17:01:40 +0200

    Generation of Simplex constraints

--- src/main/scala/concrete/generator/ProblemGenerator.scala
+++ src/main/scala/concrete/generator/ProblemGenerator.scala
@@ -1,35 +1,86 @@
 package concrete.generator;
 
 import scala.reflect.runtime.universe
+import scala.util.Try
+import org.scalameter.Quantity
 import com.typesafe.scalalogging.LazyLogging
 import concrete.Domain
 import concrete.IntDomain
 import concrete.ParameterManager
 import concrete.Problem
 import concrete.Variable
+import concrete.constraint.linear.Simplex
+import concrete.constraint.linear.SumMode
+import concrete.constraint.semantic.Clause
+import concrete.constraint.semantic.ClauseConstraint
+import concrete.constraint.semantic.PseudoBoolean
+import concrete.constraint.semantic.SAT
+import concrete.generator.constraint.Generator
 import concrete.generator.constraint.GeneratorManager
+import concrete.generator.constraint.SumGenerator
 import cspom.CSPOM
 import cspom.Statistic
 import cspom.StatisticsManager
-import cspom.TimedException
 import cspom.VariableNames
+import cspom.util.Finite
 import cspom.variable.BoolVariable
+import cspom.variable.CSPOMConstant
+import cspom.variable.CSPOMSeq
 import cspom.variable.CSPOMVariable
 import cspom.variable.IntVariable
-import cspom.variable.CSPOMSeq
-import cspom.variable.CSPOMConstant
-import concrete.generator.constraint.Generator
-import concrete.constraint.semantic.Clause
-import concrete.constraint.semantic.SAT
-import concrete.constraint.semantic.PseudoBoolean
-import concrete.generator.constraint.Var
-import concrete.constraint.semantic.SumMode
-import concrete.generator.constraint.Const
-import concrete.constraint.semantic.SumMode._
-import concrete.generator.constraint.SumGenerator
-import scala.util.Try
-import cspom.util.Finite
-import org.scalameter.Quantity
+import scala.collection.mutable.HashMap
+
+case class LinearConstraint(
+    vars: Seq[Variable], factors: Seq[Int],
+    mode: SumMode, constant: Int) extends Arc {
+
+  def size = vars.size
+
+}
+
+object Arc {
+
+  def components[T <: Arc](arcs: Seq[T]): Seq[(Set[Variable], Seq[T])] = {
+    var edges = Map[Variable, Set[T]]().withDefaultValue(Set())
+
+    for (a <- arcs; v <- a.vars) {
+      edges += v -> (edges(v) + a)
+    }
+
+    val neighb = edges.mapValues { e => e.map(_.varSet).flatten }
+
+    var visited: Set[Variable] = Set()
+    var components: Seq[Set[Variable]] = Seq()
+
+    for (v <- neighb.keys) {
+      val component = crawlVariables(v, neighb, visited)
+      if (component.nonEmpty) {
+        components +:= component
+        visited ++= component
+      }
+    }
+
+    for (vars <- components) yield {
+      (vars, vars.flatMap(edges).toSeq)
+    }
+  }
+
+  def crawlVariables(root: Variable, neighbours: Map[Variable, Set[Variable]], visited: Set[Variable]): Set[Variable] = {
+    if (visited(root)) {
+      Set()
+    } else {
+      neighbours(root).foldLeft(visited + root) {
+        (visit, n) => visit ++ crawlVariables(n, neighbours, visit)
+      }
+
+    }
+  }
+}
+
+trait Arc {
+  def vars: Seq[Variable]
+  lazy val varSet = vars.toSet
+}
 
 final class ProblemGenerator(private val pm: ParameterManager = new ParameterManager()) extends LazyLogging {
 
@@ -52,6 +103,7 @@ final class ProblemGenerator(private val pm: ParameterManager = new ParameterMan
 
       var clauses: Seq[Clause] = Seq()
       var pb: Seq[PseudoBoolean] = Seq()
+      var lin: Seq[LinearConstraint] = Seq()
 
       cspom.constraints.foreach {
         case c if c.function == 'clause =>
@@ -63,26 +115,58 @@ final class ProblemGenerator(private val pm: ParameterManager = new ParameterMan
             val negConc = neg.collect { case v: BoolVariable => Generator.cspom2concreteVar(v)(variables) }.toArray
             clauses +:= Clause(posConc, negConc)
           }
-        case constraint if constraint.function == 'pseudoboolean =>
+        case constraint if constraint.function == 'pseudoboolean && constraint.nonReified =>
 
           val (vars, varParams, constant, mode) = SumGenerator.readCSPOM(constraint)
 
           if (vars.nonEmpty) {
             val solverVariables = vars.map(Generator.cspom2concreteVar(_)(variables))
-            pb +:= PseudoBoolean(solverVariables, varParams, mode, constant)
+            pb +:= new PseudoBoolean(solverVariables, varParams, mode, constant)
           }
+
+        case constraint if constraint.function == 'sum && constraint.nonReified =>
+
+          val (vars, varParams, constant, mode) = SumGenerator.readCSPOM(constraint)
+
+          if (vars.nonEmpty) {
+            val solverVariables = vars.map(Generator.cspom2concreteVar(_)(variables))
+            lin +:= new LinearConstraint(solverVariables, varParams, mode, constant)
+          }
+
         case c =>
           val constraints = gm.generate(c, variables, vn).get
           logger.debug(s"Generating $constraints from $c")
           for (
             constraint <- constraints
           ) {
-
             problem.addConstraint(constraint)
           }
       }
 
-      SAT(clauses, pb, pm).foreach(problem.addConstraint)
+      if (pm.contains("sat4clauses")) {
+        if (pm.contains("sat4pb")) {
+          SAT(clauses, pb).foreach(problem.addConstraint)
+        } else {
+          lin ++:= pb
+          SAT(clauses, Seq()).foreach(problem.addConstraint)
+        }
+      } else {
+        clauses.map(new ClauseConstraint(_)).foreach(problem.addConstraint(_))
+        if (pm.contains("sat4pb")) {
+          SAT(Seq(), pb).foreach(problem.addConstraint)
+        } else {
+          lin ++:= pb
+        }
+      }
+
+      if (pm.contains("simplex")) {
+        Simplex(lin, pm).foreach(problem.addConstraint(_))
+      } else {
+        val sg = new SumGenerator(pm)
+        for (l <- lin) {
+          sg.general(l.vars, l.factors, l.constant, l.mode).toSeq.foreach(problem.addConstraint(_))
+        }
+      }
 
       logger.info(problem.toString(problem.initState.toState).split("\n").map(l => s"% $l").mkString("\n"))
 
