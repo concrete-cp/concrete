19dd892b2cb0fb40a1185a66cc10cfe6a64364b7
Julien Vion <julien.vion@univ-valenciennes.fr>
2017-07-28 10:59:06 +0200

    Actual 3.5 releases after more optim and debugging, upgraded Scala to 2.12.3

---
+++ src/main/scala/concrete/generator/cspompatterns/Pow.scala
@@ -0,0 +1,68 @@
+package concrete.generator.cspompatterns
+
+import cspom.CSPOM.SeqOperations
+import cspom.compiler.ConstraintCompilerNoData
+import cspom.extension.MDDRelation
+import cspom.variable.{CSPOMConstant, CSPOMSeq, SimpleExpression}
+import cspom.{CSPOM, CSPOMConstraint}
+import mdd.{JavaMap, MDD, MDD0, MDDLeaf}
+
+final object Regular extends ConstraintCompilerNoData {
+
+  override def matchBool(constraint: CSPOMConstraint[_], problem: CSPOM) = {
+    constraint.function == 'regular && constraint.result.isTrue
+  }
+
+  /**
+    * The sequence of values in array 'x' (which must all be in the range 1..S)
+    * is accepted by the DFA of 'Q' states with input 1..S and transition
+    * function 'd' (which maps (1..Q, 1..S) -> 0..Q)) and initial state 'q0'
+    * (which must be in 1..Q) and accepting states 'F' (which all must be in
+    * 1..Q).  We reserve state 0 to be an always failing state.
+    */
+
+  def compile(constraint: CSPOMConstraint[_], problem: CSPOM) = {
+    val Seq(
+    CSPOMSeq(x: Seq[SimpleExpression[Int]]@unchecked),
+    CSPOMConstant(q0),
+    CSPOMConstant.seq(f: Seq[_])) = constraint.arguments
+
+    val Some(dfa) = constraint.getParam[Map[(Any, Any), Any]]("dfa")
+
+    val values = dfa.keys.map(_._2).toSeq.distinct.to[collection.IndexedSeq] //x.map(IntExpression.implicits.iterable).toIndexedSeq
+
+    val regular = mdd(IndexedSeq.fill(x.length)(values), q0, f.toSet, dfa)
+    //
+    replaceCtr(constraint, x in new MDDRelation(regular), problem)
+  }
+
+  def mdd[T](v: IndexedSeq[Seq[T]], initState: T, finalStates: Set[T], dfa: Map[(T, T), T]): MDD = {
+    val cache = new JavaMap[(Int, T), MDD]()
+
+    def parse(depth: Int, state: T): MDD = cache.getOrElseUpdate((depth, state), {
+      if (depth >= v.length) {
+        if (finalStates(state)) {
+          MDDLeaf
+        } else {
+          MDD0
+        }
+      } else {
+
+        val trie = for {value <- v(depth)
+                        nextState <- dfa.get((state, value))
+                        subMDD: MDD = parse(depth + 1, nextState)
+                        if (subMDD.nonEmpty)} yield {
+          concrete.util.Math.any2Int(value) -> subMDD
+        }
+
+        MDD.fromTrie(trie)
+      }
+    })
+
+    parse(0, initState)
+
+  }
+
+  def selfPropagation = false
+
+}
